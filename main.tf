6bc46416029b5986cc1f4b4d7aea7085d536243a
chitra <chmanu.chinu@gmail.com>
2023-04-14 16:18:05 +0000

    tf-module-rds

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ resource "aws_rds_cluster_instance" "main" {
   identifier         = "${var.env}-rds-${count.index}"
   cluster_identifier = aws_rds_cluster.main.id
   instance_class     = var.instance_class
+   engine_version          = var.engine_version
 
 }
 
