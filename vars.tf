b008274a3c38c918d4bfbfdfb3eb43e4a7e07b3d
chitra <chmanu.chinu@gmail.com>
2023-04-14 15:32:01 +0000

    tf-module-rds

--- vars.tf
+++ vars.tf
@@ -2,4 +2,11 @@ variable "env" {}
 
 variable "subnet_ids" {}
 variable "tags" {}
-variable "database_name" {}
+variable "database_name" {
+    default = "dummy"
+}
+
+variable "backup_retention_period" {}
+variable "preffered_backup_window" {}
+variable "engine" {}
+variable "engine_version" {}
