57114be6a78958ee28faf0a4cdd09d9061087fe0
chitra <chmanu.chinu@gmail.com>
2023-04-16 08:38:33 +0000

    update rds

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_rds_cluster" "main" {
   master_password         = data.aws_ssm_parameter.pass.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = true
   db_subnet_group_name = aws_db_subnet_group.main.name
   
   tags = merge(
